8c7bec1ad23a258672d116ccf9410703a4bada4f
fb-inoue <aki.inoue@feature-branch.co.jp>
2025-03-25 05:44:48 +0900

    ページ公開

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -14,4 +14,10 @@ export default defineConfig({
   dev: {
     port: 3000,
   },
+  output: {
+    distPath: {
+      root: 'dist',
+    },
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/doq_mari/' : '/',
+  },
 });
